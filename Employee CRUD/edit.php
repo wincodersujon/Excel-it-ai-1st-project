a40441a3c45baddf7c9cdca735da2d954fcb9e2f
Sujon Biswas <66966376+wincodersujon@users.noreply.github.com>
2023-06-20 11:17:29 +0600

    Update edit.php

--- Employee CRUD/edit.php
+++ Employee CRUD/edit.php
@@ -3,6 +3,8 @@
     session_start();
  
     $id=$_GET['id'];
+    $dis_name=$_POST['dis_name'];
+    $thana_name=$_POST['thana_name'];
     $name=$_POST['name'];
     $email=$_POST['email'];
     $age=$_POST['age'];
@@ -10,7 +12,9 @@
     $designation=$_POST['designation'];
     $area_id=$_POST['area_id'];
      
-    $conn->query("update employees set name='$name', email='$email', age='$age', department='$department' ,designation='$designation' , department='$department'  where id='$id'");
+    $query=$conn->query("select employees.name,thana.thana_name,district.dis_name,employees.area_id,employees.age,employees.department,employees.designation,employees.email,employees.id from employees INNER JOIN thana
+    ON employees.area_id = thana.id INNER JOIN district
+    ON district.id = thana.district_id ;");
     $_SESSION['msg']="Employee Updated Succesfully";
     header('location:index.php');
 ?>
